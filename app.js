85718ead2eb625b2d6d52fd5a9a3524091315a00
vedatcinbat <cinbatvedat@gmail.com>
2023-12-26 13:08:23 +0300

    New Commit

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api/users', UserRoute);
 
 sequelize.sync().then(async () => {
-    console.log('Database synced successfully!');
+    console.log('Database synced!');
     app.listen(3000, () => {
         console.log('Server started on port 3000');
     })
